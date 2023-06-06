f1273f516fca998cefc2051638213f46dba6d597
Laboratório Informática <labinfo@unifil.br>
2023-06-05 21:58:20 -0300

    CommitDoidera

--- script.js
+++ script.js
@@ -1,138 +1,73 @@
-const btnCalc = document.querySelector("#btn");
+const spanOperation = document.querySelector('#span-operation');
+const selectResult = document.querySelector('.select-operation');
 
-btnCalc.addEventListener("click", function () {
-    // Primeiro Função
-    const a11 = parseFloat(document.querySelector("#a11").value);
-    const a12 = parseFloat(document.querySelector("#a12").value);
-    const a13 = parseFloat(document.querySelector("#a13").value);
-    const b1 = parseFloat(document.querySelector('#b1').value);
-
-    // Segunda Função
-
-    const a21 = parseFloat(document.querySelector("#a21").value);
-    const a22 = parseFloat(document.querySelector("#a22").value);
-    const a23 = parseFloat(document.querySelector("#a23").value);
-    const b2 = parseFloat(document.querySelector('#b2').value);
-
-    // Terceira Função
-
-    const a31 = parseFloat(document.querySelector('#a31').value);
-    const a32 = parseFloat(document.querySelector('#a32').value);
-    const a33 = parseFloat(document.querySelector('#a33').value);
-    const b3 = parseFloat(document.querySelector('#b3').value);
-
-    function gaussSolver(A, b){
-        var i, j, k, l, m;
-        //ETAPA DE ESCALONAMENTO
-        for(k = 0; k < A.length - 1; k++){
-            //procura o maior k-ésimo coeficiente em módulo
-            var max = Math.abs(A[k][k]);
-            var maxIndex = k;
-            for(i = k + 1; i < A.length; i++){
-                if(max < Math.abs(A[i][k])){
-                    max = Math.abs(A[i][k]);
-                    maxIndex = i;
-                }
-            }
-            if(maxIndex != k){
-                /*
-                 troca a equação k pela equação com o
-                 maior k-ésimo coeficiente em módulo
-                 */
-                for(j = 0; j < A.length; j++){
-                    var temp = A[k][j];
-                    A[k][j] = A[maxIndex][j];
-                    A[maxIndex][j] = temp;
-                }
-                var temp = b[k];
-                b[k] = b[maxIndex];
-                b[maxIndex] = temp;
-            }
-            //Se A[k][k] é zero, então a matriz dos coeficiente é singular
-            //det A = 0
-            if(A[k][k] == 0){
-                return null;
-            }else{
-                //realiza o escalonamento
-                for(m = k + 1; m < A.length; m++){
-                    var F = -A[m][k] / A[k][k];
-                    A[m][k] = 0; //evita uma iteração
-                    b[m] = b[m] + F * b[k];
-                    for(l = k + 1; l < A.length; l++){
-                        A[m][l] = A[m][l] + F * A[k][l];
-                    }
-                }
-            }
-        }
-        //ETAPA DE RESOLUÇÃO DO SISTEMA
-        var X = [];
-        for(i = A.length - 1; i >= 0; i--){
-            X[i] = b[i];
-            for(j = i + 1; j < A.length; j++){
-                X[i] = X[i] - X[j] * A[i][j];
-            }
-            X[i] = X[i] / A[i][i];
-        }
-        return X;
+document.getElementById('btn').addEventListener('click', function() {
+    // Obter o valor de epsilon
+    const epsilon = parseFloat(document.getElementById('epsilon').value);
+  
+    // Obter os valores dos coeficientes e termos independentes das equações
+    const a11 = parseFloat(document.getElementById('a11').value);
+    const a12 = parseFloat(document.getElementById('a12').value);
+    const a13 = parseFloat(document.getElementById('a13').value);
+    const b1 = parseFloat(document.getElementById('b1').value);
+  
+    const a21 = parseFloat(document.getElementById('a21').value);
+    const a22 = parseFloat(document.getElementById('a22').value);
+    const a23 = parseFloat(document.getElementById('a23').value);
+    const b2 = parseFloat(document.getElementById('b2').value);
+  
+    const a31 = parseFloat(document.getElementById('a31').value);
+    const a32 = parseFloat(document.getElementById('a32').value);
+    const a33 = parseFloat(document.getElementById('a33').value);
+    const b3 = parseFloat(document.getElementById('b3').value);
+  
+    // Definir os valores iniciais para as variáveis x1, x2 e x3
+    let x1 = 0;
+    let x2 = 0;
+    let x3 = 0;
+  
+    // Número máximo de iterações
+    const maxIteracoes = 100;
+  
+    // Variáveis auxiliares
+    let iteracao = 0;
+    let erro = 1;
+  
+    // Executar o método de Gauss-Seidel
+    while (iteracao < maxIteracoes && erro > epsilon) {
+      // Salvar os valores antigos de x1, x2 e x3
+      const x1Anterior = x1;
+      const x2Anterior = x2;
+      const x3Anterior = x3;
+  
+      // Calcular os novos valores de x1, x2 e x3
+      x1 = (b1 - (a12 * x2) - (a13 * x3)) / a11;
+      x2 = (b2 - (a21 * x1) - (a23 * x3)) / a22;
+      x3 = (b3 - (a31 * x1) - (a32 * x2)) / a33;
+  
+      // Calcular o erro
+      erro = Math.abs((x1 - x1Anterior) / x1) +
+        Math.abs((x2 - x2Anterior) / x2) +
+        Math.abs((x3 - x3Anterior) / x3);
+  
+      // Incrementar o número de iterações
+      iteracao++;
     }
+  
+    // Exibir o resultado
+    const output = document.getElementById('output');
+    output.innerHTML = `
+      <p>x1: ${x1}</p>
+      <p>x2: ${x2}</p>
+      <p>x3: ${x3}</p>
+      <p>Número de iterações: ${iteracao}</p>
+      <p>Erro: ${erro}</p>
+    `;
+  });
 
-    console.log(gaussSolver());
+  // Verificação de qual Operação usar
+
+  function changeOperation(){
     
-});
-
-// Eliminação de Gauss
-
-function gaussSolver(A, b){
-    var i, j, k, l, m;
-    //ETAPA DE ESCALONAMENTO
-    for(k = 0; k < A.length - 1; k++){
-        //procura o maior k-ésimo coeficiente em módulo
-        var max = Math.abs(A[k][k]);
-        var maxIndex = k;
-        for(i = k + 1; i < A.length; i++){
-            if(max < Math.abs(A[i][k])){
-                max = Math.abs(A[i][k]);
-                maxIndex = i;
-            }
-        }
-        if(maxIndex != k){
-            /*
-             troca a equação k pela equação com o
-             maior k-ésimo coeficiente em módulo
-             */
-            for(j = 0; j < A.length; j++){
-                var temp = A[k][j];
-                A[k][j] = A[maxIndex][j];
-                A[maxIndex][j] = temp;
-            }
-            var temp = b[k];
-            b[k] = b[maxIndex];
-            b[maxIndex] = temp;
-        }
-        //Se A[k][k] é zero, então a matriz dos coeficiente é singular
-        //det A = 0
-        if(A[k][k] == 0){
-            return null;
-        }else{
-            //realiza o escalonamento
-            for(m = k + 1; m < A.length; m++){
-                var F = -A[m][k] / A[k][k];
-                A[m][k] = 0; //evita uma iteração
-                b[m] = b[m] + F * b[k];
-                for(l = k + 1; l < A.length; l++){
-                    A[m][l] = A[m][l] + F * A[k][l];
-                }
-            }
-        }
-    }
-    //ETAPA DE RESOLUÇÃO DO SISTEMA
-    var X = [];
-    for(i = A.length - 1; i >= 0; i--){
-        X[i] = b[i];
-        for(j = i + 1; j < A.length; j++){
-            X[i] = X[i] - X[j] * A[i][j];
-        }
-        X[i] = X[i] / A[i][i];
-    }
-    return X;
-}
+  }
+  
